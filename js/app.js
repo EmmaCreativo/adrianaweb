5526adaa28105333eea7d4c3059c3e08a8c530af
EmmaCreativo <imac@Emmas-iMac.local>
2020-04-08 15:32:21 -0600

    creacion de metodos de cerrar menu

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
+// Metodos del menu en movil y tableta
+const cerrar_menu = document.getElementById('cerrar');
+const menu_oculto = document.getElementById('menuOculto');
+
+// cerrar
+cerrar_menu.addEventListener('click', function() {
+  menu_oculto.style.transform = "translateY(-100vh)";
+  menu_oculto.style.transition = ".5s ease-in-out";
+});
+
+// abrir
 
 
-
-
-
-
+// Navegacion de la pagina principal 
 var swiper = new Swiper('.swiper-container', {
     direction: 'vertical',
     slidesPerView: 1,
