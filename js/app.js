6808a3e5530a1aaf90ee68811488022ee8f9588b
EmmaCreativo <imac@Emmas-iMac.local>
2020-04-08 17:21:33 -0600

    pasar de gh pades a master

--- js/app.js
+++ js/app.js
@@ -1,9 +1,27 @@
+// Metodos del menu en movil y tableta
+var menu_oculto = document.querySelector('.menu-oculto');
+var menu = document.querySelector('.menu-movil');
+var cerrar = document.getElementById('cerrar');
 
+ //Funcion del menu
+function toggleMenu(event) {
+  this.classList.toggle('menu-act');
+  menu_oculto.classList.toggle('menu-activo');
+  event.preventDefault();
 
+  if (menu.classList.contains('menu-act')) {
+    console.log('hola');
+    cerrar.innerHTML = 'Cerrar';
+  } else{
+    cerrar.innerHTML = 'Menu';
+  }
+}
 
+// evento
 
+menu.addEventListener('click', toggleMenu, false);
 
-
+// Navegacion de la pagina principal 
 var swiper = new Swiper('.swiper-container', {
     direction: 'vertical',
     slidesPerView: 1,
