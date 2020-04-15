4e136b13baf9998b03566e5975db7e1ebf2cc37a
EmmaCreativo <imac@Emmas-iMac.local>
2020-04-15 14:46:49 -0600

    cambios galeria en diagramacion

--- js/estudioGaleria.js
+++ js/estudioGaleria.js
@@ -1,7 +1,7 @@
 var swiper = new Swiper('.swiper-container', {
     spaceBetween: 0,
     pagination: {
-      // el: '.swiper-pagination',
+       el: '.swiper-pagination',
       // type: 'fraction',
     },
     // navigation: {
