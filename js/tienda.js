c40c52973df4211fd56f238e48fadbb2d74c284f
EmmaCreativo <imac@Emmas-iMac.local>
2020-04-20 15:01:24 -0600

    terminar pagina tienda e iniciar responsive

--- js/tienda.js
+++ js/tienda.js
@@ -25,3 +25,55 @@ var swiper = new Swiper('.swiper-container', {
       },
     }
   });
+
+
+
+// --------------------------------------------------- Aparicion de carrito
+
+var original = 'http://127.0.0.1:5500/tienda.html';
+var carrito =document.getElementById('carritoCompra');
+var dir = window.location.href;
+//   dir.toString;
+  // console.log(dir);
+
+  function observar() {
+      if (original == dir) {
+          carrito.style.display = 'block';
+          console.log('visible');
+      } else {
+          carrito.style.display = 'none';
+      }
+  }
+
+  observar();
+
+
+
+
+
+
+  // Get the modal
+var menucarrito = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("carritoCompra");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("carrito-close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  menucarrito.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  menucarrito.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == menucarrito) {
+    menucarrito.style.display = "none";
+  }
+}
